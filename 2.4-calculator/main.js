ddf059b57b26dbd127ee95e56a876b4b615d98f4
Justin <justin.strayhorn@hotmai.com>
2016-12-08 17:02:18 -0600

    assigned variable in JS

---
+++ 2.4-calculator/main.js
@@ -0,0 +1,9 @@
+// [] Will use JS to create event actions when each button in calculator is clicked
+// [] 1. Create eventListener to allow calc to function once HTML loads.
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  var numberKeyElement = document.querySelectorAll("[data-js='numberKey']")
+  
+
+)}
