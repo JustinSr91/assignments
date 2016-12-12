6a881a321214590f6bd6ce296cd8fbf17c6d0ecd
Justin <justin.strayhorn@hotmai.com>
2016-12-12 16:18:20 -0600

    HTML and JS linked. Added poster and movie title

---
+++ 3.1-ajax/js/main.js
@@ -0,0 +1,41 @@
+// [x] Once HTML set up, establish DOM.
+// [x] Find API data from external source
+// [x] Create variable for XHR
+// [x] Open XHR ("GET")
+// [x] Set up Event Listener
+// [x] Change response from string to object
+
+// Establishing DOM
+document.addEventListener("DOMContentLoaded", function(){
+
+  var moviesSectionElement = document.querySelector("[data-js='movies']");
+  // creating xhr instance
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("GET","http://www.omdbapi.com/?t=Wedding+Crashers&y=&plot=short&r=json");
+
+  xhr.addEventListener("load", function(e){
+
+  var xhrData = this.response;
+
+  var JSONData = JSON.parse(xhrData);
+
+  var searchArray = JSONData.Search;
+  searchArray.forEach(function(movieData){
+
+    var movieArticleHTML = "<article>";
+          movieArticleHTML += "<h2>";
+            movieArticleHTML += movieData.Title;
+          movieArticleHTML += "</h2>";
+          movieArticleHTML += "<img src='";
+            movieArticleHTML += movieData.Poster;
+          movieArticleHTML += "'/>";
+        movieArticleHTML += "</article";
+    moviesSectionElement.innerHTML += movieArticleHTML;
+
+    console.log(movieArticleHTML);
+    });
+  });
+
+xhr.send();
+});
