e8da8f271876479f04faa8843d07525c81993729
Justin <justin.strayhorn@hotmai.com>
2016-12-13 13:30:07 -0600

    Starting Form creation

---
+++ 3.2-form-builder/js/main.js
@@ -0,0 +1,13 @@
+// Setting up DOM...
+document.addEventListener(DOMContentLoaded (function){
+
+
+// Linking my JS to HTML document...
+  var formElement = document.querySelector("[data-js='form']");
+
+// Creating var (instance) to request Form information...
+  var xhr = new XMLHttpRequest();
+
+
+
+})
