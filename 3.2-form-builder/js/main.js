34b9ddba43f1c4df3f4561f0276ad28fda640793
Justin <justin.strayhorn@hotmai.com>
2016-12-13 15:43:46 -0600

    form input data modified

--- 3.2-form-builder/js/main.js
+++ 3.2-form-builder/js/main.js
@@ -1,13 +1,29 @@
 // Setting up DOM...
-document.addEventListener(DOMContentLoaded (function){
-
+document.addEventListener("DOMContentLoaded", function(){
 
 // Linking my JS to HTML document...
-  var formElement = document.querySelector("[data-js='form']");
+  var formElementArray = document.querySelector("[data-js='form']");
 
 // Creating var (instance) to request Form information...
   var xhr = new XMLHttpRequest();
 
+// Grabbed url for form data...
+  xhr.open("GET", "http://json-data.herokuapp.com/forms");
 
+// creating eventListener...
+  xhr.addEventListener("load", function(e){
 
+  var xhrData = this.response;
+
+// Now I will parse JSON data...
+  var JSONData = JSON.parse(xhrData);
+  for (var i = 0; i < JSONData.length; i++){
+    var dataArray = JSONData[i];
+    console.log(dataArray);
+
+  }
+
+  });
+
+  xhr.send();
 })
