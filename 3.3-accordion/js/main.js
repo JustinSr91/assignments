33255dbccd63a4e7bec3f358a1a1dc0973e458eb
Justin <justin.strayhorn@hotmai.com>
2016-12-14 16:14:35 -0600

    added expand functionality to list elements

--- 3.3-accordion/js/main.js
+++ 3.3-accordion/js/main.js
@@ -1,7 +1,19 @@
 // adding factory call in place of DOMContentLoaded...
 
-$function(){
+$(function(){
 
+// created a var to reference the positions elements in HTML...
+  var $positionElements = $("[data-js='position']");
 
+// here, I will give a listener that will look for the click of the element on the page.
+  $positionElements.on("click", function(e){
 
-}
+// the keyword (this) will find and reference the clicked item
+    var $clickedItem = $(this);
+
+    $positionElements.filter(".paragraph__expand").removeClass("paragraph__expand");
+  //
+    $clickedItem.addClass("paragraph__expand");
+  });
+
+});
