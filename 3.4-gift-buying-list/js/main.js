4ef49e0e6d47afa48c41fb7cf253fbc5cb0e46c7
Justin <justin.strayhorn@hotmai.com>
2016-12-16 13:35:10 -0600

    added JS functionality

--- 3.4-gift-buying-list/js/main.js
+++ 3.4-gift-buying-list/js/main.js
@@ -1,4 +1,16 @@
 
 $(function(){
-  
+
+  var $body = $("body");
+
+  var $itemTextArea = $body.find("[data-js='item__textArea']");
+  var $addButton = $body.find("[data-js='form__addButton']");
+  var $itemSection = $body.find("[data-js='item__section']");
+
+  $addButton.on("click", function(e){
+
+    e.preventDefault();
+
+    var $textAreaValue = $itemTextArea.val();
+  })
 })
