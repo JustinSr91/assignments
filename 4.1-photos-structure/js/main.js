1c9a038c23a2a1b17035e0806be16e2126e8152e
Justin <justin.strayhorn@hotmai.com>
2016-12-21 14:29:50 -0600

    Completed initial set up of npm and webpack

--- 4.1-photos-structure/js/main.js
+++ 4.1-photos-structure/js/main.js
@@ -4,22 +4,131 @@ $(function(){
 
   var $body = $("body");
 
-  var $modalClose = $body.find("[data-js='modal__close']");
+  var $modalCloseBear = $body.find("[data-js='modal__close__bear']");
+  var $modalCloseDeer = $body.find("[data-js='modal__close__deer']");
+  var $modalCloseDog = $body.find("[data-js='modal__close__dog']");
+  var $modalCloseDuck = $body.find("[data-js='modal__close__duck']");
+  var $modalCloseFish = $body.find("[data-js='modal__close__fish']");
+  var $modalCloseHorse = $body.find("[data-js='modal__close__horse']");
   var $mainElement = $body.find("[data-js='main']");
-  var $imgElement = $body.find("[data-js='image']");
-  var $backButton = $body.find("[data-js='backbutton']");
+
+  var $fishImgElement = $body.find("[data-js='fishImage']");
+  var $bearImgElement = $body.find("[data-js='bearImage']");
+  var $deerImgElement = $body.find("[data-js='deerImage']");
+  var $dogImgElement = $body.find("[data-js='dogImage']");
+  var $duckImgElement = $body.find("[data-js='duckImage']");
+  var $horseImgElement = $body.find("[data-js='horseImage']");
+
+  var $backButton = $body.find("[data-js='backbutton]");
   var $albumHeading2 = $body.find("[data-js='albumHeading2']");
 
-    $imgElement.on("click", function(e){
+      $fishImgElement.on("click", function(e){
+        $albumHeading2.addClass("remove__album__heading2");
+        $mainElement.addClass("main__close");
+        $modalCloseFish.removeClass("modal__close__fish");
+        $modalCloseDuck.addClass("modal__close__duck");
+        $modalCloseHorse.addClass("modal__close__horse");
+        $modalCloseDeer.addClass("modal__close__deer");
+        $modalCloseBear.addClass("modal__close__bear");
+        $modalCloseDog.addClass("modal__close__dog");
+
+    });
+
+      $backButton.on("click", function(e){
+        $albumHeading2.removeClass("remove__album__heading2");
+        $mainElement.removeClass("main__close");
+        $modalCloseFish.addClass("modal__close__fish");
+        $modalCloseDuck.addClass("modal__close__duck");
+        $modalCloseHorse.addClass("modal__close__horse");
+        $modalCloseDeer.addClass("modal__close__deer");
+        $modalCloseBear.addClass("modal__close__bear");
+        $modalCloseDog.addClass("modal__close__dog");
+
+
+      $bearImgElement.on("click", function(e){
+        $albumHeading2.addClass("remove__album__heading2");
+        $mainElement.addClass("main__close");
+        $modalCloseBear.removeClass("modal__close__bear");
+    });
+
+      $backButton.on("click", function(e){
+        $albumHeading2.removeClass("remove__album__heading2");
+        $mainElement.removeClass("main__close");
+        $modalCloseBear.addClass("modal__close__bear");
+        $modalCloseDuck.addClass("modal__close__duck");
+        $modalCloseHorse.addClass("modal__close__horse");
+        $modalCloseDeer.addClass("modal__close__deer");
+        $modalCloseFish.addClass("modal__close__fish");
+        $modalCloseDog.addClass("modal__close__dog");
+    });
+
+      $dogImgElement.on("click", function(e){
+        $albumHeading2.addClass("remove__album__heading2");
+        $mainElement.addClass("main__close");
+        $modalCloseDog.removeClass("modal__close__dog");
+    });
+
+      $backButton.on("click", function(e){
+        $albumHeading2.removeClass("remove__album__heading2");
+        $mainElement.removeClass("main__close");
+        $modalCloseDog.addClass("modal__close__dog");
+        $modalCloseDuck.addClass("modal__close__duck");
+        $modalCloseHorse.addClass("modal__close__horse");
+        $modalCloseDeer.addClass("modal__close__deer");
+        $modalCloseFish.addClass("modal__close__fish");
+        $modalCloseBear.addClass("modal__close__bear");
+    });
+
+    $deerImgElement.on("click", function(e){
       $albumHeading2.addClass("remove__album__heading2");
       $mainElement.addClass("main__close");
-      $modalClose.removeClass("modal__close");
-  });
+      $modalCloseDeer.removeClass("modal__close__deer");
+    });
 
     $backButton.on("click", function(e){
       $albumHeading2.removeClass("remove__album__heading2");
       $mainElement.removeClass("main__close");
-      $modalClose.addClass("modal__close");
-  });
+      $modalCloseDeer.addClass("modal__close__deer");
+      $modalCloseDuck.addClass("modal__close__duck");
+      $modalCloseHorse.addClass("modal__close__horse");
+      $modalCloseFish.addClass("modal__close__fish");
+      $modalCloseBear.addClass("modal__close__bear");
+      $modalCloseDog.addClass("modal__close__dog");
+    });
+
+    $horseImgElement.on("click", function(e){
+      $albumHeading2.addClass("remove__album__heading2");
+      $mainElement.addClass("main__close");
+      $modalCloseHorse.removeClass("modal__close__horse");
+    });
+
+    $backButton.on("click", function(e){
+      $albumHeading2.removeClass("remove__album__heading2");
+      $mainElement.removeClass("main__close");
+      $modalCloseDuck.addClass("modal__close__duck");
+      $modalCloseHorse.addClass("modal__close__horse");
+      $modalCloseDeer.addClass("modal__close__deer");
+      $modalCloseFish.addClass("modal__close__fish");
+      $modalCloseBear.addClass("modal__close__bear");
+      $modalCloseDog.addClass("modal__close__dog");
+    });
+
+    $duckImgElement.on("click", function(e){
+      $albumHeading2.addClass("remove__album__heading2");
+      $mainElement.addClass("main__close");
+      $modalCloseDuck.removeClass("modal__close__duck");
+    });
+
+    $backButton.on("click", function(e){
+      $albumHeading2.removeClass("remove__album__heading2");
+      $mainElement.removeClass("main__close");
+      $modalCloseDuck.addClass("modal__close__duck");
+      $modalCloseHorse.addClass("modal__close__horse");
+      $modalCloseDeer.addClass("modal__close__deer");
+      $modalCloseFish.addClass("modal__close__fish");
+      $modalCloseBear.addClass("modal__close__bear");
+      $modalCloseDog.addClass("modal__close__dog");
+
+    });
 
 });
---
+++ 4.3-refactor-calculator/dist/bundle.js
@@ -0,0 +1,174 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	// [] Will use JS to create event actions when each button in calculator is clicked
+	// [] 1. Create eventListener to allow calc to function once HTML loads.
+	
+	document.addEventListener("DOMContentLoaded", function(){
+	
+	// CREATING VARIABLES FOR ALL CALCULATOR NUMBER KEY ELEMENTS/CLASSES IN HTML.
+	// QUERY SELECTOR PULLS ELEMENTS OVER FROM HTML AND INSERTS INTO JS FUNCTION.
+	// WILL EVENTUALLY TRY QUERY SELECTOR 'ALL' AND CREATE FOR-LOOP FOR NUMBER KEYS.
+	
+	  var numberKeyElementEqual = document.querySelector("[data-js='numberKeyEqual']");
+	  var outputKeyElement = document.querySelector("[data-js='numberKeyEqual']");
+	  numberKeyElementEqual.addEventListener("click", function(){
+	  var answerOutput = eval(outputKeyElement.textContent);
+	    outputKeyElement.textContent = answerOutput;
+	  });
+	
+	  var numberKeyElementClear = document.querySelector("[data-js='numberKeyClear']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElementClear.addEventListener("click", function(){
+	    outputKeyElement.textContent = "";
+	  });
+	
+	  var numberKeyElement7 = document.querySelector("[data-js='numberKey7']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement7.addEventListener("click", function(){
+	    outputKeyElement.textContent += 7;
+	  });
+	
+	  var numberKeyElement8 = document.querySelector("[data-js='numberKey8']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement8.addEventListener("click", function(){
+	    outputKeyElement.textContent += 8;
+	  });
+	
+	  var numberKeyElement9 = document.querySelector("[data-js='numberKey9']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement9.addEventListener("click", function(){
+	    outputKeyElement.textContent += 9;
+	  });
+	
+	  var numberKeyElementDivision = document.querySelector("[data-js='numberKeyDivision']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElementDivision.addEventListener("click", function(){
+	    outputKeyElement.textContent += "/";
+	  });
+	
+	  var numberKeyElement4 = document.querySelector("[data-js='numberKey4']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement4.addEventListener("click", function(){
+	    outputKeyElement.textContent += 4;
+	  });
+	
+	  var numberKeyElement5 = document.querySelector("[data-js='numberKey5']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement5.addEventListener("click", function(){
+	    outputKeyElement.textContent += 5;
+	  });
+	
+	  var numberKeyElement6 = document.querySelector("[data-js='numberKey6']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement6.addEventListener("click", function(){
+	    outputKeyElement.textContent += 6;
+	  });
+	
+	  var numberKeyElementX = document.querySelector("[data-js='numberKeyX']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElementX.addEventListener("click", function(){
+	    outputKeyElement.textContent += "*";
+	  });
+	
+	  var numberKeyElement1 = document.querySelector("[data-js='numberKey1']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement1.addEventListener("click", function(){
+	    outputKeyElement.textContent += 1;
+	  });
+	
+	  var numberKeyElement2 = document.querySelector("[data-js='numberKey2']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement2.addEventListener("click", function(){
+	    outputKeyElement.textContent += 2;
+	  });
+	
+	  var numberKeyElement3 = document.querySelector("[data-js='numberKey3']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement3.addEventListener("click", function(){
+	    outputKeyElement.textContent += 3;
+	  });
+	
+	  var numberKeyElementMinus = document.querySelector("[data-js='numberKeyMinus']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElementMinus.addEventListener("click", function(){
+	    outputKeyElement.textContent += "-";
+	  });
+	
+	  var numberKeyElement0 = document.querySelector("[data-js='numberKey0']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElement0.addEventListener("click", function(){
+	    outputKeyElement.textContent += 0;
+	  });
+	
+	  var numberKeyElementDecimal = document.querySelector("[data-js='numberKeyDecimal']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElementDecimal.addEventListener("click", function(){
+	    outputKeyElement.textContent += ".";
+	  });
+	
+	  var numberKeyElementPlus = document.querySelector("[data-js='numberKeyPlus']");
+	  var outputKeyElement = document.querySelector("[data-js='output__Window']");
+	  numberKeyElementPlus.addEventListener("click", function(){
+	    outputKeyElement.textContent += "+";
+	  });
+	
+	// HERE IS THE VARIABLE FOR THE OUTPUT WINDOW.
+	
+	
+	
+	  //
+	  // numberKeyElement8.addEventListener("click", function(){
+	  //   outputKeyElement.textContent += 8;
+	
+	  // });
+	
+	});
+
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
