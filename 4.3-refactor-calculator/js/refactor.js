71d1491dc4018c7fddb43ba347345f0e0b6b928a
Justin <justin.strayhorn@hotmai.com>
2016-12-28 10:51:35 -0600

    Refactored Clear and Equals button

--- 4.3-refactor-calculator/js/refactor.js
+++ 4.3-refactor-calculator/js/refactor.js
@@ -2,39 +2,42 @@
 // [x] Create event listener in JQ
 // [x] Find elements in JQ ($body.find)
 // [x] Refactor equal button
+// [x] Refactor Clear button
 
 var $ = require("jquery");
 
 $(function(){
 
-  var $body = $(".calc__body");
-  var $numberKeyElementClear = $body.find("[data-js='numberKeyClear']");
-  var $numberKeyElementEqual = $body.find("[data-js='numberKeyEqual']");
+  var $body = $("body");
+  var $clearElement = $body.find("[data-js='Clear']");
+  var $equalElement = $body.find("[data-js='Equals']");
   // var $outputKeyElement = $body.find("[data-js='numberKeyEqual']");
   var $outputWindowElement = $body.find("[data-js='output__Window']");
-
+  var $numberKeyElement = $body.find("[data-js='numberKey']");
   // Here I am calling Equal button when clicked...
-  $numberKeyElementEqual.on("click", function(e){
-    // This is giving Equal button its functionality
-    var answerOutput = eval($outputWindowElement.html());
-    // Linking output window and output with HTML...
-    $outputWindowElement.html(answerOutput);
-  })
-
-
-
-
-
-})
-
-// THE CODE BELOW IS TRIAL AND ERROR FOR ALL CALC NUMBERS. PLACED ALL NUMBERS INTO ONE DATA-JS GROUP AND RAN THE CODE BELOW. COULD NOT GET THE CODE TO SHOW EXACT NUMBER CLICKED IN OUTPUT WINDOW. INSTEAD, TOTAL GROUP OF NUMBERS INSIDE DATA-JS GROUPING SHOWS IN WINDOW. CODE ABOVE WORKS FOR ALL NUMBERS AS THEY ARE PRESSED.
-
-  // $numberKeyElement.on("click", function(e){
-  //   var $clickedNumberString = $(this);
-  //   var outputString = $outputWindowElement.text();
-  //   var numberClicked = $numberKeyElement.text();
-  //   $outputWindowElement.text(outputString + numberClicked);
-  //   console.log($numberKeyElement.text());
-  //   // $outputWindowElement.html(answerOutput);
-  //
+  // $numberKeyElementEqual.on("click", function(e){
+  //   // This is giving Equal button its functionality
+  //   var answerOutput = eval($outputWindowElement.html());
+  //   // Linking output window and output with HTML...
+  //   $outputWindowElement.html(answerOutput);
   // })
+
+    $numberKeyElement.on("click", function(e){
+      var $clickedNumberString = $(this);
+      var numberOutputString = $outputWindowElement.text();
+      var numberString = $clickedNumberString.text();
+      $outputWindowElement.text(numberOutputString + numberString);
+      // console.log($.text());
+      // $outputWindowElement.html(answerOutput);
+
+      $equalElement.on("click", function(e){
+       var total = eval($outputWindowElement.text());
+       $outputWindowElement.text(total);
+     });
+
+     $clearElement.on("click", function(){
+       var $this = $("this");
+       $this = $outputWindowElement.text("");
+     });
+   });
+});
