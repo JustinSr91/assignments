205dbf4a6eef4a9d3aebee58faef03a1a168f9a6
Justin <justin.strayhorn@hotmai.com>
2016-12-28 10:56:21 -0600

    Deleted excess comments from refactor js

--- 4.3-refactor-calculator/dist/bundle.js
+++ 4.3-refactor-calculator/dist/bundle.js
@@ -64,24 +64,14 @@
 	  var $body = $("body");
 	  var $clearElement = $body.find("[data-js='Clear']");
 	  var $equalElement = $body.find("[data-js='Equals']");
-	  // var $outputKeyElement = $body.find("[data-js='numberKeyEqual']");
 	  var $outputWindowElement = $body.find("[data-js='output__Window']");
 	  var $numberKeyElement = $body.find("[data-js='numberKey']");
-	  // Here I am calling Equal button when clicked...
-	  // $numberKeyElementEqual.on("click", function(e){
-	  //   // This is giving Equal button its functionality
-	  //   var answerOutput = eval($outputWindowElement.html());
-	  //   // Linking output window and output with HTML...
-	  //   $outputWindowElement.html(answerOutput);
-	  // })
 	
 	    $numberKeyElement.on("click", function(e){
 	      var $clickedNumberString = $(this);
 	      var numberOutputString = $outputWindowElement.text();
 	      var numberString = $clickedNumberString.text();
 	      $outputWindowElement.text(numberOutputString + numberString);
-	      // console.log($.text());
-	      // $outputWindowElement.html(answerOutput);
 	
 	      $equalElement.on("click", function(e){
 	       var total = eval($outputWindowElement.text());
