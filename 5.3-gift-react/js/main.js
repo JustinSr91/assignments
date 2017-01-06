9ed931848fdef0b5ddb1a44480fa1225283a36ce
Justin <justin.strayhorn@hotmai.com>
2017-01-05 21:55:55 -0600

    All HTML elements rendered in React

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -1,7 +1,8 @@
-// [] Determine initial state and set (placeholder)
-// [] render HTML elements
-    // [] bring over title h1 element
-    // [] bring over form element
+// [x] Determine initial state and set (placeholder)
+// [x] render HTML elements
+    // [x] bring over title h1 element
+    // [x] bring over form element
+// [] create and refine dropdown functionality
 
 
 import React from 'react'
@@ -9,17 +10,38 @@ import React from 'react'
 export default React.createClass({
   getInitialState() {
     return {
-      textAreaValue: "type the gift to buy here"
+      textAreaValue: ''
     }
 
   },
+
+  onItemAddButtonClick(e) {
+
+    e.preventDefault();
+    var currentTextAreaValue = this.state.textAreaValue;
+    var listItemElement = currentTextAreaValue[0];
+    this.refs.gift.prepend(listItemElement)
+  },
+  updateTextAreaValue: function(evt) {
+    this.setState({
+      textAreaValue: evt.target.value
+    })
+  },
+
   render() {
     return (
       <main className="main" ref="mainElement">
         <h1 className="main__h1" ref="Title">GIFTS TO BUY</h1>
         <form className="item__form" ref="textForm"></form>
-          <input className="item__textArea" ref="giftTextArea"/>
-          <input className="addButton" ref="giftAddButton"/>
+          <input className="item__textArea" type="text" ref="itemTextArea" name="giftInput" placeholder="enter name of gift here..." value={ this.state.textAreaValue } onChange={ this.updateTextAreaValue }></input>
+          <input className="addButton" type="submit" name="itemAddButton"ref="giftAddButton" value="ADD" onClick={ this. onItemAddButtonClick }></input>
+        <section className="list__section" ref="giftListSection">
+          <article>
+            <button className="check__button" ref="checkButton"></button>
+            <h2 className="h2__item" ref="gift"></h2>
+          </article>
+        </section>
+
       </main>
     )
   }
