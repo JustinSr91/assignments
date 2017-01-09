71dad63fb5eb9b92ad1409ba2fc1aea2ab578c1b
Justin <justin.strayhorn@hotmai.com>
2017-01-08 18:48:40 -0600

    modified styling of post title and date elements

--- 5.4-facebook/js/newsfeed.js
+++ 5.4-facebook/js/newsfeed.js
@@ -2,6 +2,7 @@
 // [x] Create all HTML elements
   // [x] <section> post container
   // [x] <img> profile image
+  // [] wrap <div> around h2 and h3 elements
   // [x] <h2> post title
   // [x] <h3> date of post
   // [x] <p> text content
@@ -12,10 +13,10 @@
       // h3 element (post__date)
       // p element (post__textContent)
 // [] Style HTML elements
-  // [] style section element (post__container)
-  // [] style img element (profile__img)
-  // [] style h2 element (post__title)
-  // [] style h3 element (post__date)
+  // [x] style section element (post__container)
+  // [x] style img element (profile__img)
+  // [x] style h2 element (post__title)
+  // [x] style h3 element (post__date)
   // [] style p element (post__textContent)
 
 
@@ -30,8 +31,12 @@ export default React.createClass({
         <section className="post__container">
           <img className="profile__img"
                src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-1/p200x200/12994577_1037121899695559_8791262156209967742_n.jpg?oh=555403f748c52a73e179697e4a8541ba&oe=5921E08F"></img>
-          <h2 className="post__title"> Instant Articles </h2>
-          <h3 className="post__date"> May 13, 2015 </h3>
+          <div className="postTitleDate__div">
+            <a href="#">
+            <h2 className="post__title"> Instant Articles </h2>
+            </a>
+            <h3 className="post__date"> May 13, 2015 </h3>
+          </div>
           <p className="post__textContent"> Welcome! To see these stories in the Instant Articles format, you must be on the latest version of the Facebook app for iPhone or Android.
 Learn more about this new tool for publishers at instantarticles.fb.com. </p>
 
